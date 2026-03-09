ca4e64da4088f2c6eeaf8a89eefe8e0966e84dca
NurvyyMelon <adinnaufal9@gmail.com>
2026-03-09 22:35:21 +0700

    dikarenakan tadi error karena int main terdapat dua, makanya saya menghapus salah satu dan sudah bisa berjalan

--- BMI.cpp
+++ BMI.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    return 0;
-}
+
 void inputData(float &berat, float &tinggi){
     cout << "Masukkan berat badan (kg): ";
     cin >> berat;
@@ -15,13 +13,13 @@ float hitungBMI(float berat, float tinggi){
 }
 string statusBMI(float bmi){
     if(bmi < 18.5)
-        return "Berat Badan Kurang";
+        {     return "Berat Badan Kurang";}
     else if(bmi < 25)
-        return "Berat Badan Normal";
+       { return "Berat Badan Normal";}
     else if(bmi < 30)
-        return "Berat Badan Kelebihan";
+      {  return "Berat Badan Kelebihan";}
     else
-        return "Obesitas";
+       { return "Obesitas";}
 }
 int main() {
     float berat, tinggi, bmi;
